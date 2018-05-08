c6e6353c002baecb4b34821db7a7cd9fc1b3c942
masteroflife1 <nicklesnicov@mail.ru>
2018-05-08 19:52:51 +0700

    Стандарт PSR Дамп базы для phpMyAdmin

--- api.php
+++ api.php
@@ -1,135 +1,161 @@
 <?php
-	$dblocation = '127.0.0.1';
-	$dbuser = 'root'; 
-	$dbpassw = ''; 
-	$dbname = 'catalog';
-	$reqError = false;
-	$textError = '';
+    $dblocation = '127.0.0.1';
+    $dbuser = 'root'; 
+    $dbpassw = ''; 
+    $dbname = 'catalog';
+    $reqError = false;
+    $textError = '';
 
-	$mysqli = new mysqli($dblocation, $dbuser, $dbpassw, $dbname);
-	if ($mysqli->connect_error) {
-		$textError = 'Connect Error (' . $mysqli->connect_errno . ') ' . $mysqli->connect_error;
-		$ans[] = array('result' => 1, 'reason' => $textError);
-		die(json_encode($ans));
-	}
+    $mysqli = new mysqli($dblocation, $dbuser, $dbpassw, $dbname);
+    if ($mysqli->connect_error) {
+        $textError = 'Connect Error ('. $mysqli->connect_errno . ') '
+		    . $mysqli->connect_error;
+        $ans[] = array('result' => 1, 'reason' => $textError);
+        die(json_encode($ans));
+    }
 
-	if (!empty($_POST['method'])){
-		switch ($_POST['method']) {
-			case 'getRub':
-				$result = $mysqli->query('SELECT * FROM rub');
-				break;
-			case 'getAuth':
-				$result = $mysqli->query('SELECT * FROM authors');
-				break;
-			case 'getNews':
-				$result = $mysqli->query('SELECT id,auth_id,head,announce FROM news');
-				break;
-			case 'getText':
-				if (!empty($_POST['param'])){
-					$result = $mysqli->query('SELECT id,text FROM news WHERE id='.$mysqli->real_escape_string($_POST['param']));
-				} else {
-					$reqError = true;
-					$textError = 'Empty parameter';
-				}
-				break;
-			case 'getRubNews':
-				$result = $mysqli->query('SELECT * FROM news_rub');
-				break;
-			case 'getAuthorNews':
-				if (!empty($_POST['param'])){
-					$result = $mysqli->query('SELECT head,announce,text FROM news WHERE auth_id='.$mysqli->real_escape_string($_POST['param']));
-				} else {
-					$reqError = true;
-					$textError = 'Empty parameter';
-				}
-				break;
-			case 'getNewsByRub':
-				if (!empty($_POST['param'])){
-					$result = $mysqli->query('SELECT head,announce FROM `news` a WHERE a.id in (SELECT b.new_id FROM `news_rub` b WHERE b.rub_id = '.$mysqli->real_escape_string($_POST['param']).')');
-				} else {
-					$reqError = true;
-					$textError = 'Empty parameter';
-				}
-				break;
-			case 'getAuthList':
-				$result = $mysqli->query('SELECT fio FROM authors');
-				break;
-			case 'getNewsInfo':
-				if (!empty($_POST['param'])){
-					$result = $mysqli->query('SELECT head,announce FROM news WHERE id='.$mysqli->real_escape_string($_POST['param']));
-					if ($mysqli->errno) break;
-					$result2 = $mysqli->query('SELECT name \'rubric\' FROM news_rub a INNER JOIN rub b ON a.rub_id = b.id WHERE a.new_id='.$mysqli->real_escape_string($_POST['param']) );
-				} else {
-					$reqError = true;
-					$textError = 'Empty parameter';
-				}
-				break;
-			case 'getNewsByHead':
-				if (!empty($_POST['param'])){
-					$result = $mysqli->query('SELECT head,announce,text FROM news WHERE head LIKE \'%'.$mysqli->real_escape_string($_POST['param']).'%\'');
-				} else {
-					$reqError = true;
-					$textError = 'Empty parameter';
-				}
-				break;
-			case 'getNewsByHeadRub':
-				if (!empty($_POST['param']) && !empty($_POST['param2'])){
-					$result = $mysqli->query('SELECT head,announce,text FROM news WHERE  head LIKE \'%'.$mysqli->real_escape_string($_POST['param']).'%\'  AND id IN (SELECT new_id FROM news_rub WHERE rub_id='.$mysqli->real_escape_string($_POST['param2']).')' );
-				} else {
-					$reqError = true;
-					$textError = 'Empty parameter';
-				}
-				break;
-			case 'getNewsByHeadRubIns':
-				if (!empty($_POST['param']) && !empty($_POST['param2'])){
-					$n = array($_POST['param2']);
-					$rubs = $n;
-					while (true) {
-						$s = implode(',',$n);
-						$result = $mysqli->query('SELECT id FROM rub WHERE mainid IN ('.$s.')');
-						if ($mysqli->errno) break;
-						if ($result->num_rows == 0) break;
-						$n = array();
-						while($row = $result->fetch_assoc()) $n[] = $row['id'];
-						$rubs = array_merge($rubs, $n);
-						$result->free();
-					}
-					if ($mysqli->errno) break;
-					$result = $mysqli->query('SELECT head,announce,text FROM news WHERE  head LIKE \'%'.$mysqli->real_escape_string($_POST['param']).'%\' AND id IN (SELECT new_id FROM news_rub WHERE rub_id IN ('.implode(',',$rubs).') )');
-				} else {
-					$reqError = true;
-					$textError = 'Empty parameter';
-				}
-				break;
-		
-			default:
-				$reqError = true;
-				$textError = 'Unknown method';
-				break;
-		}
-	}
-	
-	if ($reqError){
-		$ans[] = array('result' => 1, 'reason' => $textError);
-	} else {
-		if ($mysqli->errno) {
-			$textError = 'Select Error (' . $mysqli->errno . ') ' . $mysqli->error;
-			$ans[] = array('result' => 1, 'reason' => $textError);
-		} else {
-			$ans[] = array('result' => 0);
-			while($row = $result->fetch_assoc()){
-				$ans[] = $row;
-			}
-			$result->free();
-			if (isset($result2)){
-				while($row = $result2->fetch_assoc()){
-					$ans[] = $row;
-				}
-				$result2->free();
-			}
-		}
-	}
+    if (!empty($_POST['method'])) {
+        switch ($_POST['method']) {
+            case 'getRub':
+                $result = $mysqli->query('SELECT * FROM rub');
+                break;
+            case 'getAuth':
+                $result = $mysqli->query('SELECT * FROM authors');
+                break;
+            case 'getNews':
+                $result = $mysqli->query('SELECT id,auth_id,head,announce '
+				    .'FROM news');
+                break;
+            case 'getText':
+                if (!empty($_POST['param'])) {
+                    $result = $mysqli->query('SELECT id,text FROM news WHERE id='
+					    .$mysqli->real_escape_string($_POST['param']));
+                } else {
+                    $reqError = true;
+                    $textError = 'Empty parameter';
+                }
+                break;
+            case 'getRubNews':
+                $result = $mysqli->query('SELECT * FROM news_rub');
+                break;
+            case 'getAuthorNews':
+                if (!empty($_POST['param'])) {
+                    $result = $mysqli->query('SELECT head,announce,text '
+					    .'FROM news WHERE auth_id='
+						.$mysqli->real_escape_string($_POST['param']));
+                } else {
+                    $reqError = true;
+                    $textError = 'Empty parameter';
+                }
+                break;
+            case 'getNewsByRub':
+                if (!empty($_POST['param'])) {
+                    $result = $mysqli->query('SELECT head,announce FROM `news` a '
+					    .'WHERE a.id in (SELECT b.new_id FROM `news_rub` b '
+						.'WHERE b.rub_id = '
+						.$mysqli->real_escape_string($_POST['param']).')');
+                } else {
+                    $reqError = true;
+                    $textError = 'Empty parameter';
+                }
+                break;
+            case 'getAuthList':
+                $result = $mysqli->query('SELECT fio FROM authors');
+                break;
+            case 'getNewsInfo':
+                if (!empty($_POST['param'])) {
+                    $result = $mysqli->query('SELECT head,announce FROM news '
+					    .'WHERE id='.$mysqli->real_escape_string($_POST['param']));
+                    if ($mysqli->errno) break;
+                    $result2 = $mysqli->query('SELECT name \'rubric\' '
+					    .'FROM news_rub a INNER JOIN rub b ON a.rub_id = b.id '
+						.'WHERE a.new_id='
+						.$mysqli->real_escape_string($_POST['param']) );
+                } else {
+                    $reqError = true;
+                    $textError = 'Empty parameter';
+                }
+                break;
+            case 'getNewsByHead':
+                if (!empty($_POST['param'])) {
+                    $result = $mysqli->query('SELECT head,announce,text '
+					.'FROM news WHERE head LIKE \'%'
+					.$mysqli->real_escape_string($_POST['param']).'%\'');
+                } else {
+                    $reqError = true;
+                    $textError = 'Empty parameter';
+                }
+                break;
+            case 'getNewsByHeadRub':
+                if (!empty($_POST['param']) && !empty($_POST['param2'])) {
+                    $result = $mysqli->query('SELECT head,announce,text '
+					    .'FROM news WHERE  head LIKE \'%'
+						.$mysqli->real_escape_string($_POST['param'])
+						.'%\'  AND id IN (SELECT new_id '
+						.'FROM news_rub WHERE rub_id='
+						.$mysqli->real_escape_string($_POST['param2']).')' );
+                } else {
+                    $reqError = true;
+                    $textError = 'Empty parameter';
+                }
+                break;
+            case 'getNewsByHeadRubIns':
+                if (!empty($_POST['param']) && !empty($_POST['param2'])) {
+                    $n = array($_POST['param2']);
+                    $rubs = $n;
+                    while (true) {
+                        $s = implode(',',$n);
+                        $result = $mysqli->query('SELECT id '
+						    .'FROM rub WHERE mainid IN ('.$s.')');
+                        if ($mysqli->errno) break;
+                        if ($result->num_rows == 0) break;
+                        $n = array();
+                        while($row = $result->fetch_assoc()) {
+						    $n[] = $row['id'];
+						}
+                        $rubs = array_merge($rubs, $n);
+                        $result->free();
+                    }
+                    if ($mysqli->errno) break;
+                    $result = $mysqli->query('SELECT head,announce,text '
+					    .'FROM news WHERE  head LIKE \'%'
+						.$mysqli->real_escape_string($_POST['param'])
+						.'%\' AND id IN (SELECT new_id '
+						.'FROM news_rub WHERE rub_id IN ('
+						.implode(',',$rubs).') )');
+                } else {
+                    $reqError = true;
+                    $textError = 'Empty parameter';
+                }
+                break;
+        
+            default:
+                $reqError = true;
+                $textError = 'Unknown method';
+                break;
+        }
+    }
+    
+    if ($reqError) {
+        $ans[] = array('result' => 1, 'reason' => $textError);
+    } else {
+        if ($mysqli->errno) {
+            $textError = 'Select Error ('.$mysqli->errno.') '.$mysqli->error;
+            $ans[] = array('result' => 1, 'reason' => $textError);
+        } else {
+            $ans[] = array('result' => 0);
+            while($row = $result->fetch_assoc()){
+                $ans[] = $row;
+            }
+            $result->free();
+            if (isset($result2)){
+                while($row = $result2->fetch_assoc()){
+                    $ans[] = $row;
+                }
+                $result2->free();
+            }
+        }
+    }
 
-	echo json_encode($ans);
-	$mysqli->close();
-?>
+    echo json_encode($ans);
+    $mysqli->close();
--- index.php
+++ index.php
@@ -11,61 +11,62 @@
 <script type="text/javascript">
 var d;
 function getTable(d){
-	var row;
-	var code;
-	var prkeys = '';
-	var keys;
-	var n = d.length;
-	for(var j = 1; j<n; j++){
-		row = d[j];
-		keys = '';
-		for (var i in row){
-			keys += '<th>' + i + '</th>';
-		}
-		if (keys != prkeys){
-			code += '<tr>' + keys + '</tr>'
-		}
-		prkeys = keys;
-		code += '<tr>';
-		for (var i in row){
-			code += '<td>' + row[i] + '</td>';
-		}
-		code += '</tr>';
-	}
-	$("#tbl").html(code);
+    var row;
+    var code;
+    var prkeys = '';
+    var keys;
+    var n = d.length;
+    for(var j = 1; j<n; j++){
+        row = d[j];
+        keys = '';
+        for (var i in row){
+            keys += '<th>' + i + '</th>';
+        }
+        if (keys != prkeys){
+            code += '<tr>' + keys + '</tr>'
+        }
+        prkeys = keys;
+        code += '<tr>';
+        for (var i in row){
+            code += '<td>' + row[i] + '</td>';
+        }
+        code += '</tr>';
+    }
+    $("#tbl").html(code);
 }
 function sendAjx(){
-	var dt = 'method=' + $("#reqMethod").val() + '&param=' + $("#reqParam").val() + '&param2=' + $("#reqParam2").val();
-	$.ajax({
-		type: "POST",
-		url: "api.php",
-		data: dt,
-		success: function(msg){
-			d = JSON.parse(msg);
-			if (d.length > 1) {
-				$("#data").text('');
-				getTable(d);
-			} else {
-				$("#tbl").html('');
-				$("#data").text("Данные: " + msg);
-			}
-		}
-	});
+    var dt = 'method=' + $("#reqMethod").val() + '&param=' 
+	    + $("#reqParam").val() + '&param2=' + $("#reqParam2").val();
+    $.ajax({
+        type: "POST",
+        url: "api.php",
+        data: dt,
+        success: function(msg){
+            d = JSON.parse(msg);
+            if (d.length > 1) {
+                $("#data").text('');
+                getTable(d);
+            } else {
+                $("#tbl").html('');
+                $("#data").text("Данные: " + msg);
+            }
+        }
+    });
 }
 </script>
 <select id="reqMethod">
-	<option value="getRub">Таблица рубрик</option>
-	<option value="getAuth">Таблица авторов</option>
-	<option value="getNews">Таблица новостей (без текста)</option>
-	<option value="getText">Текст новости по id</option>
-	<option value="getRubNews">Таблица рубрик для новости</option>
-	<option value="getAuthorNews">Все новости автора по id</option>
-	<option value="getNewsByRub">Список новостей по id рубрики</option>
-	<option value="getAuthList">Список авторов</option>
-	<option value="getNewsInfo">Информация о новостях по id</option>
-	<option value="getNewsByHead">Поиск новости по названию</option>
-	<option value="getNewsByHeadRub">Поиск новости по названию и рубрике</option>
-	<option value="getNewsByHeadRubIns">Поиск новости по названию и рубрике, вк. вложенные</option>
+    <option value="getRub">Таблица рубрик</option>
+    <option value="getAuth">Таблица авторов</option>
+    <option value="getNews">Таблица новостей (без текста)</option>
+    <option value="getText">Текст новости по id</option>
+    <option value="getRubNews">Таблица рубрик для новости</option>
+    <option value="getAuthorNews">Все новости автора по id</option>
+    <option value="getNewsByRub">Список новостей по id рубрики</option>
+    <option value="getAuthList">Список авторов</option>
+    <option value="getNewsInfo">Информация о новостях по id</option>
+    <option value="getNewsByHead">Поиск новости по названию</option>
+    <option value="getNewsByHeadRub">Поиск новости по названию и рубрике</option>
+    <option value="getNewsByHeadRubIns">Поиск новости по названию и рубрике, вк. вложенные</option>
 </select>
 <input type="text" id="reqParam">
 <input type="text" id="reqParam2">
